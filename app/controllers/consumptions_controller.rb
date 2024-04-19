a8d1d1f8d1b7cffa63ed7d246090a5572030323d
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-19 14:24:45 +0200

    aquatrak

--- app/controllers/consumptions_controller.rb
+++ app/controllers/consumptions_controller.rb
@@ -1,2 +1,21 @@
 class ConsumptionsController < ApplicationController
+  def index
+    @consumptions = Consumption.where(user_id: params[:user_id])
+    render json: @consumptions
+  end
+
+  def create
+    @consumption = Consumption.new(consumption_params)
+    if @consumption.save
+      render json: @consumption, status: :created
+    else
+      render json: @consumption.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def consumption_params
+    params.require(:consumption).permit(:user_id, :quantity, :recorded_at)
+  end
 end
